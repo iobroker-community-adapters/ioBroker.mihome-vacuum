096a9b2e2493828d706f967fb5c8ad6a29a031ae
Bluefox <dogafox@gmail.com>
2017-01-23 21:01:15 +0100

    (bluefox) add power level control

--- main.js
+++ main.js
@@ -21,6 +21,18 @@ adapter.on('stateChange', function (id, state) {
     // output to parser
     var command = id.split('.').pop();
 
+    if (command === 'level') {
+        state.val = parseInt(state.val, 10);
+        if (state.val < 1)  state.val = 1;
+        if (state.val > 3)  state.val = 3;
+        if (commands['level' + state.val]) {
+            sendCommand(commands['level' + state.val], function () {
+                adapter.setForeignState(adapter.namespace + '.level', state.val, true);
+            });
+        } else {
+            adapter.log.warn('Command level' + state.val + ' is not configured');
+        }
+    } else
     if (command === 'state') {
         if (state.val === 'true' || state.val === true || state.val === '1' || state.val === 1) {
             if (commands.start) {
@@ -78,12 +90,25 @@ function sendPing() {
         }
     }, 3000);
 
-    server.send(commands.ping, adapter.config.port, adapter.config.ip, function (err) {
-        if (err) adapter.log.error('Cannot send ping: ' + err)
-    });
+    try {
+        server.send(commands.ping, adapter.config.port, adapter.config.ip, function (err) {
+            if (err) adapter.log.error('Cannot send ping: ' + err)
+        });
+    } catch (e) {
+        adapter.log.warn('Cannot send ping: ' + e);
+        clearTimeout(pingTimeout);
+        pingTimeout = null;
+        if (connected) {
+            connected = false;
+            adapter.log.info('Disconnect');
+            adapter.setState('info.connection', false, true);
+        }
+    }
+
 }
 
 function str2hex(str) {
+    str = str.replace(/\s/g, '');
     var buf = new Buffer(str.length / 2);
 
     for (var i = 0; i < str.length / 2; i++) {
@@ -93,10 +118,15 @@ function str2hex(str) {
 }
 
 function sendCommand(cmd, callback) {
-    server.send(cmd, adapter.config.port, adapter.config.ip, function (err) {
-        if (err) adapter.log.error('Cannot send command: ' + err);
+    try {
+        server.send(cmd, adapter.config.port, adapter.config.ip, function (err) {
+            if (err) adapter.log.error('Cannot send command: ' + err);
+            if (typeof callback === 'function') callback(err);
+        });
+    } catch (err) {
+        adapter.log.warn('Cannot send command_: ' + err);
         if (typeof callback === 'function') callback(err);
-    });
+    }
 }
 
 function main() {
@@ -106,10 +136,13 @@ function main() {
     adapter.config.pingInterval = parseInt(adapter.config.pingInterval, 10) || 20000;
     commands = {
         ping:   str2hex('21310020ffffffffffffffffffffffffffffffffffffffffffffffffffffffff'),
-        start:  adapter.config.start ? str2hex(adapter.config.start) : '',
-        pause:  adapter.config.pause ? str2hex(adapter.config.pause) : '',
-        home:   adapter.config.home  ? str2hex(adapter.config.home)  : '',
-        find:   adapter.config.find  ? str2hex(adapter.config.find)  : ''
+        start:  adapter.config.start  ? str2hex(adapter.config.start)  : '',
+        pause:  adapter.config.pause  ? str2hex(adapter.config.pause)  : '',
+        home:   adapter.config.home   ? str2hex(adapter.config.home)   : '',
+        find:   adapter.config.find   ? str2hex(adapter.config.find)   : '',
+        level1: adapter.config.level1 ? str2hex(adapter.config.level1) : '',
+        level2: adapter.config.level2 ? str2hex(adapter.config.level2) : '',
+        level3: adapter.config.level3 ? str2hex(adapter.config.level3) : ''
     };
 
     server.on('error', function (err) {
