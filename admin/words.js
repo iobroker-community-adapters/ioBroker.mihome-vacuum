230ec823c895141f79e119a95189d5f66d9df87b
MeisterTR <rensen26@gmail.com>
2018-01-18 20:01:14 +0100

    1.0.1
    admin3

--- admin/words.js
+++ admin/words.js
@@ -1,44 +1,77 @@
+/*global systemDictionary:true */
+'use strict';
+
 systemDictionary = {
-  "MiHome-vacuum adapter settings": {
-    "de": "MiHome-vacuum Adapter Einstellungen",
-    "ru": "Настройки драйвера MiHome пылесоса"
-  },
-  "IP address:": {
-    "en": "IP address: ",
-    "de": "IP-Adresse des Roboters: ",
-    "ru": "IP адрес пылесоса: "
-  },
-  "Vacuum port:": {
-    "en": "Vacuum port: ",
-    "de": "Port des Roboters: ",
-    "ru": "Порт пылесоса: "
-  },
-  "Own port:": {
-    "en": "Own port: ",
-    "de": "Eigener Port: ",
-    "ru": "Собственный порт: "
-  },
-  "Token:": {
-    "en": "Token: ",
-    "de": "Token: ",
-    "ru": "Токен: "
-  },
-  "Request Intervall:": {
-    "en": "Request Intervall in ms: ",
-    "de": "Abfrage Intervall in ms: ",
-    "ru": "Request Intervall in ms: "
-  },
-  "settings": {
-    "en": "settings",
-    "de": "Einstellungen",
-    "ru": "settings"
-  },
-  "Send own commands":{
-    "en": "Send own commands:",
-    "de": "Sende eigene Kommandos:",
-    "ru": "Send own commands:"
-  },
-  "Invalid token length. Expected 32 or 96 HEX chars.": {
-    "de": "Ungültige Tokenlänge. 32 oder 96 HEX-Zeichen erwartet."   
-  }
+    "IP address:": {        "en": "IP address: ",        "de": "IP-Adresse des Roboters: ",        "ru": "Айпи адрес:",        "pt": "Endereço de IP:",        "nl": "IP adres:",        "fr": "Adresse IP:",        "it": "Indirizzo IP:",        "es": "Dirección IP:",        "pl": "Adres IP:"
+    },
+
+    "Invalid token length. Expected 32 or 96 HEX chars.": {        "en": "Invalid token length. Expected 32 or 96 HEX chars.",        "de": "Ungültige Tokenlänge. 32 oder 96 HEX-Zeichen erwartet.",        "ru": "Недопустимая длина токена. Ожидаемые 32 или 96 символов HEX.",        "pt": "Comprimento de token inválido. Esperava 32 ou 96 caracteres HEX.",        "nl": "Ongeldige tokenlengte. Verwachte 32 of 96 HEX-tekens.",        "fr": "Longueur de jeton incorrecte Caractères HEX attendus 32 ou 96.",        "it": "Lunghezza del token non valida. Previsti 32 o 96 caratteri HEX.",        "es": "Longitud de token inválida. Se esperan 32 o 96 caracteres HEX.",        "pl": "Nieprawid?owa d?ugo?? tokenu. Oczekiwano 32 lub 96 znak�w HEX."
+    },
+    "MiHome-vacuum adapter settings": {        "en": "MiHome-vacuum adapter settings",        "de": "MiHome-vacuum Adapter Einstellungen",        "ru": "Настройки адаптера MiHome-vacuum",        "pt": "Configura��es do adaptador MiHome-v�cuo",
+        "nl": "MiHome-vacu�m adapterinstellingen",
+        "fr": "MiHome-param�tres d'adaptateur de vide",
+        "it": "Impostazioni dell'adattatore per vuoto MiHome",
+        "es": "Configuraci�n del adaptador MiHome-vacuum",
+        "pl": "Ustawienia adaptera MiHome-vacuum"
+    },
+    "Own port:": {        "en": "Own port: ",        "de": "Eigener Port: ",
+        "ru": "Собственный порт:",
+        "pt": "Porta própria:",
+        "nl": "Eigen poort:",
+        "fr": "Propre port:",
+        "it": "Porto proprio:",
+        "es": "Puerto propio:",
+        "pl": "W?asny port:"
+    },
+    "Request Intervall:": {
+        "en": "Request Intervall in ms: ",
+        "de": "Abfrage Intervall in ms: ",
+        "ru": "Запросить интервал в мс:",
+        "pt": "Solicitar Intervall em ms:",
+        "nl": "Interval aanvragen in ms:",
+        "fr": "Demander Intervall en ms:",
+        "it": "Richiesta Intervall in ms:",
+        "es": "Solicitar Intervall en ms:",
+        "pl": "Popro? Intervall w ms:"
+    },
+    "Send own commands": {        "en": "Send own commands:",        "de": "Sende eigene Kommandos:",        "ru": "Отправить собственные команды:",        "pt": "Mandar comandos pr�prios:",
+        "nl": "Stuur eigen commando's:",
+        "fr": "Envoyer ses propres commandes:",
+        "it": "Invia i propri comandi:",
+        "es": "Envía tus propios comandos:",
+        "pl": "Wyślij własne polecenia:"
+    },
+    "Token:": {
+        "en": "Token: ",
+        "de": "Token: ",
+        "ru": "Знак:",
+        "pt": "Símbolo:"
+        , "nl": "token:",
+        "fr": "Jeton:",
+        "it": "Gettone:",
+        "es": "Simbólico:",
+        "pl": "Znak:"
+    },
+    "Vacuum port:": {
+        "en": "Vacuum port: ",
+        "de": "Port des Roboters: ",
+        "ru": "Вакуумный порт:",
+        "pt": "Porta de vácuo:",
+        "nl": "Vacuümpoort:",
+        "fr": "Port de vide:",
+        "it": "Porta di vuoto:",
+        "es": "Puerto de vacío:",
+        "pl": "Port próżniowy:"
+    },
+    "settings": {
+        "en": "settings",
+        "de": "Einstellungen",
+        "ru": "настройки",
+        "pt": "configura��es",
+        "nl": "instellingen",
+        "fr": "param�tres",
+        "it": "impostazioni",
+        "es": "ajustes",
+        "pl": "ustawienia"
+    },
 };
