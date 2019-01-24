9bdf0fe509d3ba5feb0bd447ce14f5f9145864d0
ldittmar <ldittmar.privat@gmail.com>
2019-01-24 15:06:04 +0100

    Add gulp auto translation
    This pr add gulp auto translation and some missing translations.
    
    For informations about gulp automatic translation (german): https://forum.iobroker.net/viewtopic.php?f=24&t=21208#p222837

--- admin/words.js
+++ admin/words.js
@@ -2,158 +2,18 @@
 'use strict';
 
 systemDictionary = {
-    "title": {
-        "en": "adapter for control of Xiaomi vacuum cleaner V1 and S5",
-        "de": "Adapter zur Steuerung von Xiaomi Staubsauger V1 und S5",
-        "ru": "адаптер для управления пылесосом V1 и S5 Xiaomi",
-        "pt": "Adaptador para controle da aspiradora Xiaomi V1 e S5",
-        "nl": "adapter voor aansturing van Xiaomi-stofzuiger V1 en S5",
-        "fr": "adaptateur pour le contrôle de l'aspirateur Xiaomi V1 et S5",
-        "it": "adattatore per il controllo dell'aspirapolvere Xiaomi V1 e S5",
-        "es": "adaptador para el control de la aspiradora Xiaomi V1 y S5",
-        "pl": "adapter do sterowania odkurzacza Xiaomi V1 i S5"
-    },
-    "IP address:": {
-        "en": "IP address:",
-        "de": "IP Adresse:",
-        "ru": "Айпи адрес:",
-        "pt": "Endereço de IP:",
-        "nl": "IP adres:",
-        "fr": "Adresse IP:",
-        "it": "Indirizzo IP:",
-        "es": "Dirección IP:",
-        "pl": "Adres IP:"
-    },
-    "connection settings": {
-        "en": "connection settings",
-        "de": "Verbindungseinstellungen",
-        "ru": "Настройки соединения",
-        "pt": "configurações de conexão",
-        "nl": "verbindingsinstellingen",
-        "fr": "paramètres de connexion",
-        "it": "impostazioni di connessione",
-        "es": "configuración de conexión",
-        "pl": "ustawienia połączenia"
-    },
-    "additional settings": {
-        "en": "additional settings",
-        "de": "zusätzliche Einstellungen",
-        "ru": "дополнительные настройки",
-        "pt": "Configurações adicionais",
-        "nl": "aanvullende instellingen",
-        "fr": "paramètres additionnels",
-        "it": "altre impostazioni",
-        "es": "ajustes adicionales",
-        "pl": "dodatkowe ustawienia"
-    },
-    "Invalid token length. Expected 32 or 96 HEX chars.": {
-        "en": "Invalid token length. Expected 32 or 96 HEX chars.",
-        "de": "Ungültige Tokenlänge Erwartete 32 oder 96 HEX-Zeichen.",
-        "ru": "Недопустимая длина токена. Ожидаемые 32 или 96 символов HEX.",
-        "pt": "Comprimento de token inválido. Esperava 32 ou 96 caracteres HEX.",
-        "nl": "Ongeldige tokenlengte. Verwachte 32 of 96 HEX-tekens.",
-        "fr": "Longueur de jeton incorrecte Caractères HEX attendus 32 ou 96.",
-        "it": "Lunghezza del token non valida. Previsti 32 o 96 caratteri HEX.",
-        "es": "Longitud de token inválida. Se esperan 32 o 96 caracteres HEX.",
-        "pl": "Nieprawidłowa długość tokenu. Oczekiwano 32 lub 96 znaków HEX."
-    },
-    "MiHome-vacuum adapter settings": {
-        "en": "MiHome-vacuum adapter settings",
-        "de": "MiHome-Vakuumadaptereinstellungen",
-        "ru": "Настройки адаптера MiHome-vacuum",
-        "pt": "Configurações do adaptador MiHome-vácuo",
-        "nl": "MiHome-vacuüm adapterinstellingen",
-        "fr": "MiHome-paramètres d'adaptateur de vide",
-        "it": "Impostazioni dell'adattatore per vuoto MiHome",
-        "es": "Configuración del adaptador MiHome-vacuum",
-        "pl": "Ustawienia adaptera MiHome-vacuum"
-    },
-    "Own port:": {
-        "en": "Own port:",
-        "de": "Eigener Port:",
-        "ru": "Собственный порт:",
-        "pt": "Porta própria:",
-        "nl": "Eigen poort:",
-        "fr": "Propre port:",
-        "it": "Porto proprio:",
-        "es": "Puerto propio:",
-        "pl": "Własny port:"
-    },
-    "Request Intervall": {
-        "en": "Request Intervall in ms ",
-        "de": "Abfrage Intervall in ms ",
-        "ru": "Запросить интервал в мс",
-        "pt": "Solicitar Intervall em ms",
-        "nl": "Interval aanvragen in ms",
-        "fr": "Demander Intervall en ms",
-        "it": "Richiesta Intervall in ms",
-        "es": "Solicitar Intervall en ms",
-        "pl": "Popro? Intervall w ms"
-    },
-    "Send own commands": {
-        "en": "Send own commands",
-        "de": "Sende eigene Befehle",
-        "ru": "Отправить собственные команды",
-        "pt": "Mandar comandos próprios",
-        "nl": "Stuur eigen commando's",
-        "fr": "Envoyer ses propres commandes",
-        "it": "Invia i tuoi comandi",
-        "es": "Enviar los propios comandos",
-        "pl": "Wyślij własne polecenia"
-    },
-    "Token": {
-        "en": "token",
-        "de": "Token",
-        "ru": "Знак",
-        "pt": "Símbolo",
-        "nl": "token",
-        "fr": "Jeton",
-        "it": "Gettone",
-        "es": "Simbólico",
-        "pl": "Znak"
-    },
-    "Vacuum port:": {
-        "en": "Vacuum port: ",
-        "de": "Port des Roboters: ",
-        "ru": "Вакуумный порт:",
-        "pt": "Porta de vácuo:",
-        "nl": "Vacuümpoort:",
-        "fr": "Port de vide:",
-        "it": "Porta di vuoto:",
-        "es": "Puerto de vacío:",
-        "pl": "Port próżniowy:"
-    },
-    "settings": {
-        "en": "settings",
-        "de": "Einstellungen",
-        "ru": "настройки",
-        "pt": "configurações",
-        "nl": "instellingen",
-        "fr": "paramètres",
-        "it": "impostazioni",
-        "es": "ajustes",
-        "pl": "ustawienia"
-    },
-    "add a state for Alexa": {
-        "en": "add a state for Alexa",
-        "de": "Füge einen Status für Alexa hinzu",
-        "ru": "добавить информацию о пользователе Alexa",
-        "pt": "adicione um estado para Alexa",
-        "nl": "voeg een staat toe voor Alexa",
-        "fr": "ajouter un état pour Alexa",
-        "it": "aggiungi uno stato per Alexa",
-        "es": "agregar un estado para Alexa",
-        "pl": "dodaj stan dla Alexy"
-    },
-    "Resume paused zonecleaning with start button": {
-        "en": "Resume paused zonecleaning with start button",
-        "de": "Zonenreinigung nach pausierung fortsetzen",
-        "ru": "Возобновление приостановки zonecleaning с кнопкой запуска",
-        "pt": "Retomar o zonecleaning pausado com o botão Iniciar",
-        "nl": "Pauzeer zonecleaning opnieuw met de startknop",
-        "fr": "Reprendre le nettoyage en mode pause avec le bouton Démarrer",
-        "it": "Riprendi zonecleaning in pausa con il pulsante di avvio",
-        "es": "Reanudar la zona de limpieza en pausa con el botón de inicio",
-        "pl": "Wznów wstrzymanie procesu oczyszczania za pomocą przycisku Start"
-    }
+    "IP address:": {                                 "en": "IP address:",                                     "de": "IP Adresse:",                                     "ru": "Айпи адрес:",                                     "pt": "Endereço de IP:",                                 "nl": "IP adres:",                                       "fr": "Adresse IP:",                                     "it": "Indirizzo IP:",                                   "es": "Dirección IP:",                                   "pl": "Adres IP:",                                       "zh-cn": "IP地址："},
+    "Invalid token length. Expected 32 or 96 HEX chars.": {"en": "Invalid token length. Expected 32 or 96 HEX chars.", "de": "Ungültige Tokenlänge Erwartete 32 oder 96 HEX-Zeichen.", "ru": "Недопустимая длина токена. Ожидаемые 32 или 96 символов HEX.", "pt": "Comprimento de token inválido. Esperava 32 ou 96 caracteres HEX.", "nl": "Ongeldige tokenlengte. Verwachte 32 of 96 HEX-tekens.", "fr": "Longueur de jeton incorrecte Caractères HEX attendus 32 ou 96.", "it": "Lunghezza del token non valida. Previsti 32 o 96 caratteri HEX.", "es": "Longitud de token inválida. Se esperan 32 o 96 caracteres HEX.", "pl": "Nieprawidłowa długość tokenu. Oczekiwano 32 lub 96 znaków HEX.", "zh-cn": "令牌长度无效。"},
+    "MiHome-vacuum adapter settings": {              "en": "MiHome-vacuum adapter settings",                  "de": "MiHome-Vakuumadaptereinstellungen",               "ru": "Настройки адаптера MiHome-vacuum",                "pt": "Configurações do adaptador MiHome-vácuo",         "nl": "MiHome-vacuüm adapterinstellingen",               "fr": "MiHome-paramètres d'adaptateur de vide",          "it": "Impostazioni dell'adattatore per vuoto MiHome",   "es": "Configuración del adaptador MiHome-vacuum",       "pl": "Ustawienia adaptera MiHome-vacuum",               "zh-cn": "MiHome真空适配器设置"},
+    "Own port:": {                                   "en": "Own port:",                                       "de": "Eigener Port:",                                   "ru": "Собственный порт:",                               "pt": "Porta própria:",                                  "nl": "Eigen poort:",                                    "fr": "Propre port:",                                    "it": "Porto proprio:",                                  "es": "Puerto propio:",                                  "pl": "Własny port:",                                    "zh-cn": "自己的港口："},
+    "Request Intervall": {                           "en": "Request Intervall in ms ",                        "de": "Abfrage Intervall in ms ",                        "ru": "Запросить интервал в мс",                         "pt": "Solicitar Intervall em ms",                       "nl": "Interval aanvragen in ms",                        "fr": "Demander Intervall en ms",                        "it": "Richiesta Intervall in ms",                       "es": "Solicitar Intervall en ms",                       "pl": "Popro? Intervall w ms",                           "zh-cn": "以毫秒为单位请求Intervall"},
+    "Resume paused zonecleaning with start button": {"en": "Resume paused zonecleaning with start button",    "de": "Zonenreinigung nach pausierung fortsetzen",       "ru": "Возобновление приостановки zonecleaning с кнопкой запуска", "pt": "Retomar o zonecleaning pausado com o botão Iniciar", "nl": "Pauzeer zonecleaning opnieuw met de startknop",   "fr": "Reprendre le nettoyage en mode pause avec le bouton Démarrer", "it": "Riprendi zonecleaning in pausa con il pulsante di avvio", "es": "Reanudar la zona de limpieza en pausa con el botón de inicio", "pl": "Wznów wstrzymanie procesu oczyszczania za pomocą przycisku Start", "zh-cn": "使用开始按钮恢复暂停的区域清理"},
+    "Send own commands": {                           "en": "Send own commands",                               "de": "Sende eigene Befehle",                            "ru": "Отправить собственные команды",                   "pt": "Mandar comandos próprios",                        "nl": "Stuur eigen commando's",                          "fr": "Envoyer ses propres commandes",                   "it": "Invia i tuoi comandi",                            "es": "Enviar los propios comandos",                     "pl": "Wyślij własne polecenia",                         "zh-cn": "发送自己的命令"},
+    "Token": {                                       "en": "token",                                           "de": "Token",                                           "ru": "Знак",                                            "pt": "Símbolo",                                         "nl": "token",                                           "fr": "Jeton",                                           "it": "Gettone",                                         "es": "Simbólico",                                       "pl": "Znak",                                            "zh-cn": "代币"},
+    "Vacuum port:": {                                "en": "Vacuum port: ",                                   "de": "Port des Roboters: ",                             "ru": "Вакуумный порт:",                                 "pt": "Porta de vácuo:",                                 "nl": "Vacuümpoort:",                                    "fr": "Port de vide:",                                   "it": "Porta di vuoto:",                                 "es": "Puerto de vacío:",                                "pl": "Port próżniowy:",                                 "zh-cn": "真空口："},
+    "adapter for control of Xiaomi vacuum cleaner V1 and S5": {"en": "adapter for control of Xiaomi vacuum cleaner V1 and S5", "de": "Adapter zur Steuerung von Xiaomi Staubsauger V1 und S5", "ru": "адаптер для управления пылесосом V1 и S5 Xiaomi", "pt": "Adaptador para controle da aspiradora Xiaomi V1 e S5", "nl": "adapter voor aansturing van Xiaomi-stofzuiger V1 en S5", "fr": "adaptateur pour le contrôle de l'aspirateur Xiaomi V1 et S5", "it": "adattatore per il controllo dell'aspirapolvere Xiaomi V1 e S5", "es": "adaptador para el control de la aspiradora Xiaomi V1 y S5", "pl": "adapter do sterowania odkurzacza Xiaomi V1 i S5", "zh-cn": "用于控制小米吸尘器V1和S5的适配器"},
+    "add a state for Alexa": {                       "en": "add a state for Alexa",                           "de": "Füge einen Status für Alexa hinzu",               "ru": "добавить информацию о пользователе Alexa",        "pt": "adicione um estado para Alexa",                   "nl": "voeg een staat toe voor Alexa",                   "fr": "ajouter un état pour Alexa",                      "it": "aggiungi uno stato per Alexa",                    "es": "agregar un estado para Alexa",                    "pl": "dodaj stan dla Alexy",                            "zh-cn": "为Alexa添加状态"},
+    "additional settings": {                         "en": "additional settings",                             "de": "zusätzliche Einstellungen",                       "ru": "дополнительные настройки",                        "pt": "Configurações adicionais",                        "nl": "aanvullende instellingen",                        "fr": "paramètres additionnels",                         "it": "altre impostazioni",                              "es": "ajustes adicionales",                             "pl": "dodatkowe ustawienia",                            "zh-cn": "其他设置"},
+    "connection settings": {                         "en": "connection settings",                             "de": "Verbindungseinstellungen",                        "ru": "Настройки соединения",                            "pt": "configurações de conexão",                        "nl": "verbindingsinstellingen",                         "fr": "paramètres de connexion",                         "it": "impostazioni di connessione",                     "es": "configuración de conexión",                       "pl": "ustawienia połączenia",                           "zh-cn": "连接设置"},
+    "settings": {                                    "en": "settings",                                        "de": "Einstellungen",                                   "ru": "настройки",                                       "pt": "configurações",                                   "nl": "instellingen",                                    "fr": "paramètres",                                      "it": "impostazioni",                                    "es": "ajustes",                                         "pl": "ustawienia",                                      "zh-cn": "设置"},
 };
